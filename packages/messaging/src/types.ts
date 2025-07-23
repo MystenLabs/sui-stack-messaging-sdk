b161e68fcdd8e23ef8a2e0692c8962d591bd7d9b
nikos-kitmeridis <nikos.kitmeridis@mystenlabs.com>
2025-07-23 10:50:54 +0300

    SEINT-1905: SDK Client scaffolding - WIP

---
+++ packages/messaging/src/types.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import {ClientWithExtensions, Experimental_CoreClient, Experimental_SuiClientTypes} from '@mysten/sui/experimental';
+
+export interface MessagingPackageConfig {
+  packageId: string;
+  memberCapType: string;
+}
+
+export type MessagingCompatibleClient = ClientWithExtensions<{
+  core: Experimental_CoreClient;
+}>;
+
+type MessagingOwnedObjects = Omit<Experimental_SuiClientTypes.GetOwnedObjectsOptions, 'type'>;
+
+export type ChannelMembershipsRequest = MessagingOwnedObjects;
